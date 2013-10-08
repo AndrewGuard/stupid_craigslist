59ed3842aa9896bb4b5f3601a2422e4b759f3ae7
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-08 18:19:44 -0500

    Greasy things are up

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,16 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+get '/auto_parts' do
+  erb :auto_parts
+end
+
+get '/greasy_thing' do
+  erb :greasy_thing
+end
+
+get '/wheels' do
+  erb :wheels
+end
